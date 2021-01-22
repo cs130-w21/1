2b71f0aae91e0dc2cd48ef3cbc87d2b0239ca712
Alexander Ma <alexanderzma@hotmail.com>
2021-01-21 20:05:52 -0800

    Ran prettier

--- src/DaemonExec.ts
+++ src/DaemonExec.ts
@@ -4,39 +4,44 @@ import Docker = require('dockerode')
 const docker = new Docker()
 
 const listImages = async () => {
-	try{
+	try {
 		const images = await docker.listImages({})
 		return images
-	} catch(err) {
+	} catch (err) {
 		console.log(err)
 		return []
 	}
 }
 
 const listContainers = async () => {
-	try{
+	try {
 		const containers = await docker.listContainers()
 		return containers
-	} catch(err) {
+	} catch (err) {
 		console.log(err)
 		return []
 	}
 }
 
 const importImage = async (name: string) => {
-	try{
+	try {
 		const stream = await docker.pull(name)
 		return stream
-	} catch(err) {
+	} catch (err) {
 		console.log(err)
 	}
 }
 
 const createContainer = async (image: string, command: string[]) => {
-	try{
-		const container = await docker.createContainer({Image: image, Cmd: command, AttachStdout: true, AttachStderr: true})
+	try {
+		const container = await docker.createContainer({
+			Image: image,
+			Cmd: command,
+			AttachStdout: true,
+			AttachStderr: true,
+		})
 		return container
-	} catch(err) {
+	} catch (err) {
 		console.log(err)
 		return null
 	}
@@ -61,23 +66,24 @@ const createContainer = async (image: string, command: string[]) => {
 const runCommand = async (name: string, image: string, command: string[]) => {
 	//create helper function to run command to avoid duplication
 	const onFinished = (err: any) => {
-		if(err)
-			console.log(err)
-		createContainer(image, command).then( async (container: any) => {
+		if (err) console.log(err)
+		createContainer(image, command).then(async (container: any) => {
 			//setup stream
-			const stream = await container.attach({stream: true, stdout: true, stderr: true})
+			const stream = await container.attach({
+				stream: true,
+				stdout: true,
+				stderr: true,
+			})
 			const writeStream = fs.createWriteStream(`./${name}.out`)
 			stream.pipe(writeStream)
 
 			//start container
 			container.start(async (err: any) => {
-				if(err)
-					console.log(err)
+				if (err) console.log(err)
 				else {
 					console.log('started')
-					container.wait(async (err: any,data: any) => {
-						if(err)
-							console.log(err)
+					container.wait(async (err: any, data: any) => {
+						if (err) console.log(err)
 						console.log('container end: ', data)
 						await container.remove()
 						console.log('removed')
@@ -90,10 +96,16 @@ const runCommand = async (name: string, image: string, command: string[]) => {
 	//callback for determining progress of image fetch
 	const onProgress = (event: any) => {
 		console.log(event.status)
-		if(event.progressDetail && event.progressDetail.current && event.progressDetail.total){
-			console.log(`${event.progressDetail.current}/${event.progressDetail.total} `)
+		if (
+			event.progressDetail &&
+			event.progressDetail.current &&
+			event.progressDetail.total
+		) {
+			console.log(
+				`${event.progressDetail.current}/${event.progressDetail.total} `,
+			)
 		}
-		if(event.progress){
+		if (event.progress) {
 			console.log(event.progress)
 		}
 	}
@@ -101,7 +113,11 @@ const runCommand = async (name: string, image: string, command: string[]) => {
 	//get the image list
 	const images = await listImages()
 
-	if(images.findIndex((e: any) => e.RepoTags.findIndex((el: string) => el == image) > -1) < 0){
+	if (
+		images.findIndex(
+			(e: any) => e.RepoTags.findIndex((el: string) => el == image) > -1,
+		) < 0
+	) {
 		//if image is not fetched, fetch it (search only works if version is in image)
 		console.log(`Fetching image: ${image}`)
 		const stream = await importImage(image)
@@ -178,4 +194,4 @@ const runCommand = async (name: string, image: string, command: string[]) => {
 
 runCommand('name', 'ubuntu:latest', ['/bin/ls'])
 
-module.exports = {listContainers, listImages, importImage, runCommand}
+module.exports = { listContainers, listImages, importImage, runCommand }
