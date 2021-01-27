60ad1c4acd2eb44ecfef2bb8498b5b1eb3e6df2b
Alexander Ma <alexanderzma@hotmail.com>
2021-01-27 12:47:52 -0800

    Documentation

--- src/DaemonExec.ts
+++ src/DaemonExec.ts
@@ -1,18 +1,42 @@
+/**
+ * Returns information about the images
+ * @param docker - The docker daemon to pull information from
+ * @returns an array of ImageInfo
+ */
 export async function listImages(docker: any) {
 	const images = await docker.listImages({})
 	return images
 }
 
+/**
+ * Returns information about the containers
+ * @param docker - The docker daemon to pull information from
+ * @returns an array of ContainerInfo
+ */
 export async function listContainers(docker: any) {
 	const containers = await docker.listContainers()
 	return containers
 }
 
+/**
+ * Starts the import of an image
+ * @param docker - The docker daemon with which the image will be stored
+ * @param name - The name of the image to pull
+ * @returns a ReadableStream to track the progress of the import
+ */
 export async function importImage(docker: any, name: string) {
 	const stream = await docker.pull(name)
 	return stream
 }
 
+/**
+ * Creates a Docker container
+ * @param docker - the docker daemon that controls the container
+ * @param image - the image the container uses
+ * @param command - an array that holds the command for the container
+ * @param volumePairs - an array of string pairs that represents the volumes
+ * @returns the Container
+ */
 export async function createContainer(
 	docker: any,
 	image: string,
@@ -45,6 +69,15 @@ export async function createContainer(
 	return container
 }
 
+/**
+ * Attaches input, output, and error streams to a container
+ * Default is stdin, stdout, and stderr
+ * @param container - the container to attach
+ * @param stdinStream - the input stream
+ * @param stdoutStream - the output stream
+ * @param stderrStream - the error strean
+ * @returns a ReadWriteStream for the container
+ */
 export async function attachStreams(
 	container: any,
 	stdinStream = process.stdin,
@@ -63,10 +96,18 @@ export async function attachStreams(
 	return stream
 }
 
+/**
+ * Stops the container
+ * @params container - the container to stop
+ */
 export async function stopContainer(container: any) {
 	await container.stop()
 }
 
+/**
+ * Removes the container
+ * @params container - the container to remove
+ */
 export async function removeContainer(container: any) {
 	await container.remove()
 }
