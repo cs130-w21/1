3f492e48cc64602d0f90bbb3aeda2ce3c4120b93
Rohit Vighne <rohit.vighne@gmail.com>
2021-02-03 00:11:49 -0800

    Finish unit tests for ZeroconfDaemon
    Added an evil hack function `mockEmitter`
     - Does black magic to return a Frankenstein mock object
     - Mock uses builtin implementation of EventEmitter
    
    Remove a dumb suggestion from myself

--- test/ZeroconfDaemon.test.ts
+++ test/ZeroconfDaemon.test.ts
@@ -11,18 +11,32 @@ const MOCK_SERVICE_NAME = 'Junknet Unit Test'
 const MOCK_SERVER_HOST = '127.0.0.1'
 const MOCK_SERVER_PORT = 1337
 
+// Parameters are unknown because those types are not exported.
+function mockEmitter<T extends EventEmitter>(
+	mockImplementation?: unknown,
+	opts?: unknown,
+) {
+	// Casting to never silences both type checker and ESLint.
+	const original = mock<T>(mockImplementation as never, opts as never)
+	return new Proxy(new EventEmitter(), {
+		get(target, prop) {
+			return Reflect.has(target, prop)
+				? Reflect.get(target, prop)
+				: Reflect.get(original, prop)
+		},
+	}) as typeof original
+}
+
 describe('publishServer', () => {
-	// TODO: instead of faking address(), create a real NodeJS server for mocking
+	const zeroconf = mock<Bonjour>()
+	zeroconf.publish.mockImplementation((options) => {
+		const service = mockEmitter<Service>(options)
+		setImmediate(() => service.emit('up'))
+		return service
+	})
 
 	it('publishes the right service type and name', async () => {
 		// Arrange
-		const zeroconf = mock<Bonjour>()
-		zeroconf.publish.mockImplementation((options) => {
-			const service = Object.assign(new EventEmitter(), mock<Service>(options))
-			setImmediate(() => service.emit('up'))
-			return service
-		})
-
 		const server = mock<Server>()
 		server.address.mockReturnValue(mock<AddressInfo>())
 
@@ -41,13 +55,6 @@ describe('publishServer', () => {
 
 	it('publishes the right socket address', async () => {
 		// Arrange
-		const zeroconf = mock<Bonjour>()
-		zeroconf.publish.mockImplementation((options) => {
-			const service = Object.assign(new EventEmitter(), mock<Service>(options))
-			setImmediate(() => service.emit('up'))
-			return service
-		})
-
 		const server = mock<Server>()
 		const address = mock<AddressInfo>({
 			address: MOCK_SERVER_HOST,
@@ -68,5 +75,21 @@ describe('publishServer', () => {
 		expect(service.port).toBe(MOCK_SERVER_PORT)
 	})
 
-	it.todo('stops publishing when the server stops listening')
+	it('stops publishing when the server stops listening', async () => {
+		// Arrange
+		const server = mockEmitter<Server>()
+		server.address.mockReturnValue(mock<AddressInfo>())
+
+		// Act
+		const service = await publishServer(
+			zeroconf,
+			SERVICE_TYPE,
+			MOCK_SERVICE_NAME,
+			server,
+		)
+		server.emit('close')
+
+		// Assert
+		expect(service.stop).toHaveBeenCalled()
+	})
 })
