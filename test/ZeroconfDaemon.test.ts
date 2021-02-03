71d80a18003d2f298e63f92a6732f7e9272c414e
Rohit Vighne <rohit.vighne@gmail.com>
2021-02-02 22:01:39 -0800

    Add stubs for ZeroconfDaemon unit tests

---
+++ test/ZeroconfDaemon.test.ts
@@ -0,0 +1,5 @@
+describe('publishServer', () => {
+	it.todo('publishes the right socket address')
+	it.todo('waits to resolve until the service is published')
+	it.todo('stops publishing when the server stops listening')
+})
