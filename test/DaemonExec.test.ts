1a19157464e1fefae7c64e4461fc2ace7397b94b
Alexander Ma <alexanderzma@hotmail.com>
2021-02-09 23:15:43 -0800

    Testing docker image pull

---
+++ test/DaemonExec.test.ts
@@ -0,0 +1,27 @@
+import * as Docker from 'dockerode'
+import {
+	//	VolumeDefinition,
+	listImages,
+	//        listContainers,
+	//        importImage,
+	ensureImageImport,
+	//        createContainer,
+	//        attachStreams,
+	//        stopContainer,
+	//        removeContainer,
+} from '../src/DaemonExec'
+
+const docker = new Docker()
+jest.setTimeout(300000)
+
+describe('DaemonExec', () => {
+	it('pulls a specified image', async () => {
+		await ensureImageImport(docker, 'ubuntu:18.04')
+		const imageInfos: Docker.ImageInfo[] = await listImages(docker)
+		expect(
+			imageInfos.some((info: Docker.ImageInfo) =>
+				info.RepoTags.some((tag: string) => tag === 'ubuntu:18.04'),
+			),
+		).toBeTruthy()
+	})
+})
