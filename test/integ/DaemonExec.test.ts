82bd08de473b2d4c206f50ab3efac46d81bfd94e
Alexander Ma <alexanderzma@hotmail.com>
2021-02-19 14:17:35 -0800

    Some minor changes. Changed names of constants and files. Changed conditions of tests.

--- test/integ/DaemonExec.test.ts
+++ test/integ/DaemonExec.test.ts
@@ -21,10 +21,10 @@ interface ExpectedStatus {
 	StatusCode: number
 }
 
-const docker = new Docker()
-const testImage = 'ubuntu:18.04'
-const bogusImage = 'lskjflskjflsdkfjslkfjsdlfsdjflksdj'
-const inputFile = 'package.json'
+const DOCKER = new Docker()
+const TEST_IMAGE = 'ubuntu:18.04'
+const BOGUS_IMAGE = 'lskjflskjflsdkfjslkfjsdlfsdjflksdj'
+const INPUT_FILE = 'package.json'
 
 describe('DaemonExec', () => {
 	let container: Docker.Container
@@ -33,40 +33,40 @@ describe('DaemonExec', () => {
 	let errorFile: string
 
 	it('pulls a new image', async () => {
-		const initialInfos = await listImages(docker)
+		const initialInfos = await listImages(DOCKER)
 		if (
 			initialInfos.some((info) =>
-				info.RepoTags.some((tag) => tag === testImage),
+				info.RepoTags.some((tag) => tag === TEST_IMAGE),
 			)
 		) {
 			// if image is already imported, remove it
-			const image = docker.getImage(testImage)
+			const image = DOCKER.getImage(TEST_IMAGE)
 			await image.remove()
 		}
 
-		await ensureImageImport(docker, testImage)
-		const newInfos = await listImages(docker)
+		await ensureImageImport(DOCKER, TEST_IMAGE)
+		const newInfos = await listImages(DOCKER)
 
 		expect(
-			newInfos.some((info) => info.RepoTags.some((tag) => tag === testImage)),
+			newInfos.some((info) => info.RepoTags.some((tag) => tag === TEST_IMAGE)),
 		).toBeTruthy()
 	}, 60000)
 
 	it('pulls an existing image', async () => {
-		await ensureImageImport(docker, testImage)
-		const newInfos = await listImages(docker)
+		await ensureImageImport(DOCKER, TEST_IMAGE)
+		const newInfos = await listImages(DOCKER)
 
 		expect(
-			newInfos.some((info) => info.RepoTags.some((tag) => tag === testImage)),
+			newInfos.some((info) => info.RepoTags.some((tag) => tag === TEST_IMAGE)),
 		).toBeTruthy()
 	})
 
 	it('fails pulling a nonexistent image', async () => {
-		await expect(ensureImageImport(docker, bogusImage)).rejects.toThrow()
+		await expect(ensureImageImport(DOCKER, BOGUS_IMAGE)).rejects.toThrow()
 	})
 
 	it('creates a specified container', async () => {
-		container = await createContainer(docker, testImage, ['/bin/cat'], [])
+		container = await createContainer(DOCKER, TEST_IMAGE, ['/bin/cat'], [])
 		expect(container.id).toBeDefined()
 	})
 
@@ -75,9 +75,9 @@ describe('DaemonExec', () => {
 
 		tempDir = await fs.mkdtemp(join(tmpdir(), 'test'))
 
-		const stdinStream: NodeJS.ReadableStream = createReadStream(inputFile)
+		const stdinStream: NodeJS.ReadableStream = createReadStream(INPUT_FILE)
 
-		outputFile = resolve(tempDir, inputFile)
+		outputFile = resolve(tempDir, INPUT_FILE)
 		const stdoutStream: NodeJS.WritableStream = createWriteStream(outputFile)
 
 		errorFile = resolve(tempDir, 'error')
@@ -89,6 +89,9 @@ describe('DaemonExec', () => {
 			stdoutStream,
 			stderrStream,
 		)
+
+		// This is to get rid of a warning in Jest
+		// stream should always be defined at this point in the code
 		expect(stream).toBeDefined()
 	})
 
@@ -102,12 +105,12 @@ describe('DaemonExec', () => {
 	})
 
 	it('writes the correct data to the streams', async () => {
-		const original = await fs.readFile(inputFile)
+		const original = await fs.readFile(INPUT_FILE)
 		const copy = await fs.readFile(outputFile)
-		expect(original).toEqual(copy)
+		expect(original.equals(copy)).toBeTruthy()
 
 		const error = await fs.readFile(errorFile)
-		expect(Buffer.byteLength(error)).toEqual(0)
+		expect(Buffer.byteLength(error)).toBe(0)
 	})
 
 	it('removes a specified container', async () => {
@@ -118,14 +121,14 @@ describe('DaemonExec', () => {
 	it('creates and runs containers with volumes and gives correct outputs', async () => {
 		// make volumes from current directory and temporary directory
 		const volumes: VolumeDefinition[] = [
-			{ fromPath: process.cwd(), toPath: '/test' },
-			{ fromPath: tempDir, toPath: '/test2' },
+			{ fromPath: process.cwd(), toPath: '/input' },
+			{ fromPath: tempDir, toPath: '/output' },
 		]
 
 		const volumeContainer = await createContainer(
-			docker,
-			testImage,
-			['/bin/cp', `/test/${inputFile}`, `/test2/${inputFile}`],
+			DOCKER,
+			TEST_IMAGE,
+			['/bin/cp', `/input/${INPUT_FILE}`, `/output/${INPUT_FILE}`],
 			volumes,
 		)
 
@@ -133,31 +136,31 @@ describe('DaemonExec', () => {
 		await volumeContainer.wait()
 
 		// volumes should have the correct files copied over
-		const original = await fs.readFile(inputFile)
-		const copy = await fs.readFile(resolve(tempDir, inputFile))
-		expect(original).toEqual(copy)
+		const original = await fs.readFile(INPUT_FILE)
+		const copy = await fs.readFile(resolve(tempDir, INPUT_FILE))
+		expect(original.equals(copy)).toBeTruthy()
 
 		await removeContainer(volumeContainer)
 	})
 
 	it('detects and stops a running container', async () => {
 		const unendingContainer = await createContainer(
-			docker,
-			testImage,
+			DOCKER,
+			TEST_IMAGE,
 			['/bin/cat', '/dev/urandom'],
 			[],
 		)
 
 		// start the containers and see if detected
 		await unendingContainer.start()
-		const containersBefore = await listContainers(docker)
+		const containersBefore = await listContainers(DOCKER)
 		expect(
 			containersBefore.some((cont) => cont.Id === unendingContainer.id),
 		).toBeTruthy()
 
 		// stop the container and see if detected
 		await stopContainer(unendingContainer)
-		const containersAfter = await listContainers(docker)
+		const containersAfter = await listContainers(DOCKER)
 		expect(
 			containersAfter.some((cont) => cont.Id === unendingContainer.id),
 		).toBeFalsy()
