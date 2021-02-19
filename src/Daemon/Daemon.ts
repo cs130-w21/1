d18f450d0bc993cbe10c22a8f382efc8e6a12060
Paramjot Singh <5233968+pjot726@users.noreply.github.com>
2021-02-18 17:58:32 -0800

    Passes linting errors

--- src/Daemon/Daemon.ts
+++ src/Daemon/Daemon.ts
@@ -1,33 +1,35 @@
 /* Imports for SSH 'server implementation */
-import * as ssh2 from 'ssh2' 
+import * as ssh2 from 'ssh2'
 import * as net from 'net'
+import * as fs from 'fs'
 /* Variables for hostkeys */
-var fs = require('fs');
-//var crypto = require('crypto');
-//var inspect = require('util').inspect;
+// var crypto = require('crypto');
+// var inspect = require('util').inspect;
 
-//var ssh2 = require('ssh2');
-//var utils = ssh2.utils;
+// var ssh2 = require('ssh2');
+// var utils = ssh2.utils;
 
 export function createDaemon(): net.Server {
-
 	// Create server object
 	const server = new ssh2.Server(
-		// SSH authentication information 	 
-		{hostKeys: [fs.readFileSync('host.key')]},
+		// SSH authentication information
+		{ hostKeys: [fs.readFileSync('host.key')] },
 		// Handle client
-		function(client : any) { 
+		(client: ssh2.Connection) => {
 			// Successful connection
-			console.log("Hello World! Client connected!");
+			console.log('Hello World! Client connected!')
 
-			//Client authentication (Currently dummy)
-			client.on('authentication', function(ctx : any) {ctx.accept()});
-		//Specify port being listened on
-		}).listen(0, '127.0.0.1', function() {
-  		console.log('Listening.');
-  		// Work-around
-  		(server as net.Server).listening = true;
-	});
-	
-	return server as net.Server;
+			// Client authentication (Currently dummy)
+			client.on('authentication', (ctx: ssh2.AuthContext) => {
+				ctx.accept()
+			})
+			// Specify port being listened on
+		},
+	).listen(0, '127.0.0.1', () => {
+		console.log('Listening.')
+		// Work-around
+		;(server as net.Server).listening = true
+	})
+
+	return server as net.Server
 }
