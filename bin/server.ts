60d1aa69707f762c4f2c98bbe6203652ab30ecee
Rohit Vighne <rohit.vighne@gmail.com>
2021-02-20 11:19:27 -0800

    Pass Dockerode instance from server program
    "Linearize" the control flow in server.ts
    Builds successfully but completely untested.

--- bin/server.ts
+++ bin/server.ts
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
 
 import bonjour from 'bonjour'
+import Dockerode from 'dockerode'
+
 import { AddressInfo } from 'net'
 import { hostname } from 'os'
+import { once } from 'events'
 
 import { SERVICE_TYPE, publishServer, createDaemon } from '../src'
 
-const zeroconf = bonjour()
+async function start(): Promise<void> {
+	const zeroconf = bonjour()
+	const docker = new Dockerode()
 
-const daemon = createDaemon()
-daemon.on('stream', (_, headers) => console.log(headers))
-daemon.listen(() => {
+	const daemon = createDaemon(docker)
+	daemon.listen()
+
+	await once(daemon, 'listening')
 	const addr = daemon.address() as AddressInfo
 	console.info(addr)
 
-	publishServer(
+	const service = await publishServer(
 		zeroconf,
 		SERVICE_TYPE,
 		`${hostname()}'s Junknet Server #${addr.port}`,
 		daemon,
 	)
-		.then(console.log)
-		.catch(console.error)
-})
+	console.log(service)
+}
+
+start().catch(console.error)
