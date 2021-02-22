4475e109f19381c1d36a6830b50e2dcfd4a429fe
Rohit Vighne <rohit.vighne@gmail.com>
2021-02-21 16:11:49 -0800

    Better workarounds for yargs bugs

--- bin/server.ts
+++ bin/server.ts
@@ -12,38 +12,44 @@ import { once } from 'events'
 import { SERVICE_TYPE, publishServer, createDaemon, dockerRunJob } from '../src'
 
 async function start(): Promise<void> {
-	/**
-	 * Modeled after sshd(8) where possible, see https://man.openbsd.org/sshd.
-	 * A few bugs, because yargs sucks:
-	 * - if the p option is passed multiple times, it makes it an array instead of a number.
-	 * - the h option can be passed without an argument and it doesn't complain.
-	 */
-	const { argv } = yargs(process.argv.slice(2))
+	// Modeled after sshd(8) where possible, see https://man.openbsd.org/sshd.
+	const { argv: opts } = yargs(process.argv.slice(2))
 		.options({
 			h: {
+				// yargs bug: requiresArg has no effect on array-valued options.
+				requiresArg: true,
 				nargs: 1,
 				array: true,
 				string: true,
 				default: 'host.key',
+				// TODO: more graceful failure when file can't be read.
 				coerce: (files: string[]) => files.map((file) => readFileSync(file)),
 			},
 			p: {
 				requiresArg: true,
 				number: true,
+				// By default, listen on any port.
 				default: 0,
+				// yargs bug: an option passed multiple times automatically becomes an array.
+				coerce: (number: number | number[]) =>
+					Array.isArray(number) ? number.pop() : number,
 			},
 		})
+		// yargs bug: strict by itself doesn't prevent unwanted positional arguments.
+		.demandCommand(0, 0)
 		.strict()
+
+	console.log(opts)
 
 	const runJob = dockerRunJob(new Dockerode())
 	const daemon = createDaemon((request, channel) => {
 		console.log(request)
 		return runJob(request, channel)
-	}, argv.h)
+	}, opts.h)
 
 	daemon.on('error', console.error)
 	daemon.on('connection', (_, ...info) => console.log(...info))
-	daemon.listen(argv.p)
+	daemon.listen(opts.p)
 
 	await once(daemon, 'listening')
 	const addr = daemon.address() as AddressInfo
