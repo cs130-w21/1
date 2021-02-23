9916b5e9f859a49d65f5d1659da55becb660710c
Yash Lala <yashlala@gmail.com>
2021-02-22 21:42:55 -0800

    Rename "Parser" code to CommandLineController.

---
+++ test/unit/CommandLineController.test.ts
@@ -0,0 +1,24 @@
+/**
+ * Tests for the Command-Line Argument Parser
+ */
+
+import * as yargs from 'yargs'
+import * as cli from '../../src/Controller/CLIController'
+
+describe('Parser', () => {
+	// Don't exit the process on failure. 
+	// This would abort our tests. 
+	yargs.exitProcess(false)
+
+	it('interprets short options', () => {
+		const argv: readonly string[] = [
+			'0', '1', '2', '-f', 'mf', '-i', 'di',
+		]
+		const results = cli.interpretArgv(argv)
+		expect(results.makefile).toBe('mf')
+		expect(results.docker_image).toBe('di')
+	})
+
+	yargs.exitProcess(true)
+})
+
