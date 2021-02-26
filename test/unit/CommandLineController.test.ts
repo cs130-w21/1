1f8b00fab455f18ff17c120b219a56d5f2738565
Yash Lala <yashlala@gmail.com>
2021-02-26 11:56:20 -0800

    Add tests for Command Line Controller.

--- test/unit/CommandLineController.test.ts
+++ test/unit/CommandLineController.test.ts
@@ -2,20 +2,76 @@
  * Tests for the Command-Line Argument Parser
  */
 
-import * as yargs from 'yargs'
+// When yargs encounters an invalid option, it'll automatically invoke process.exit().
+// This is a problem, because it would muck up our testing framework.
+// We could handle this by explicitly passing a fail() callback to yargs,
+// but this would override the very-nice default handler. In short, testing
+// this scenario would require a lot of code.
+//
+// I don't think that the effort is worth it, because our code is just a
+// wrapper around yargs anyways (I trust their tests).
+
 import * as cli from '../../src/Controller/CommandLineController'
 
 describe('Parser', () => {
-	// Don't exit the process on failure.
-	// This would abort our tests.
-	yargs.exitProcess(false)
-
 	it('interprets short options', () => {
-		const argv: readonly string[] = ['0', '1', '2', '-f', 'mf', '-i', 'di']
+		const argv: readonly string[] = [
+			'0',
+			'1',
+			'2',
+			'-f',
+			'mf',
+			'-i',
+			'di',
+			'-t',
+			't',
+		]
 		const results = cli.interpretArgv(argv)
 		expect(results.makefile).toBe('mf')
 		expect(results.docker_image).toBe('di')
+		expect(results.target).toBe('t')
 	})
 
-	yargs.exitProcess(true)
+	it('interprets --option=var style long options', () => {
+		const argv: readonly string[] = [
+			'0',
+			'1',
+			'2',
+			'3',
+			'--makefile=mf',
+			'--docker-image=di',
+			'--target=t',
+		]
+		const results = cli.interpretArgv(argv)
+		expect(results.makefile).toBe('mf')
+		expect(results.docker_image).toBe('di')
+		expect(results.target).toBe('t')
+	})
+
+	it('interprets "--option var" style long options', () => {
+		const argv: readonly string[] = [
+			'0',
+			'1',
+			'2',
+			'3',
+			'--makefile',
+			'mf',
+			'--docker-image',
+			'di',
+			'--target',
+			't',
+		]
+		const results = cli.interpretArgv(argv)
+		expect(results.makefile).toBe('mf')
+		expect(results.docker_image).toBe('di')
+		expect(results.target).toBe('t')
+	})
+
+	it('sets correct default options', () => {
+		const argv: readonly string[] = ['0', '1']
+		const results = cli.interpretArgv(argv)
+		expect(results.makefile).toBe('Makefile')
+		expect(results.docker_image).toBe('ubuntu:18.04')
+		expect(results.target).toBeNull()
+	})
 })
