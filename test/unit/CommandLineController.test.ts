0ac0340042ec834cef0ea34e38641212382de6fe
Yash Lala <yashlala@gmail.com>
2021-02-27 12:43:43 -0800

    Add positional-argument tests for the CLI Controller.

--- test/unit/CommandLineController.test.ts
+++ test/unit/CommandLineController.test.ts
@@ -2,76 +2,71 @@
  * Tests for the Command-Line Argument Parser
  */
 
-// When yargs encounters an invalid option, it'll automatically invoke process.exit().
-// This is a problem, because it would muck up our testing framework.
-// We could handle this by explicitly passing a fail() callback to yargs,
-// but this would override the very-nice default handler. In short, testing
-// this scenario would require a lot of code.
+// When yargs encounters an invalid option (eg. -z), it'll automatically invoke
+// process.exit(). This is a problem, because it would muck up our testing
+// framework. We could handle this by explicitly passing a fail() callback to
+// yargs, but this would override the very-nice default handler. In short,
+// testing this scenario would require a lot of code.
 //
 // I don't think that the effort is worth it, because our code is just a
 // wrapper around yargs anyways (I trust their tests).
 
 import * as cli from '../../src/Controller/CommandLineController'
 
-describe('Parser', () => {
+describe('CommandLineController', () => {
 	it('interprets short options', () => {
-		const argv: readonly string[] = [
-			'0',
-			'1',
-			'2',
-			'-f',
-			'mf',
-			'-i',
-			'di',
-			'-t',
-			't',
-		]
+		const argv: readonly string[] = ['-f', 'myMakefile.make', 'ubuntu:18.04']
 		const results = cli.interpretArgv(argv)
-		expect(results.makefile).toBe('mf')
-		expect(results.docker_image).toBe('di')
-		expect(results.target).toBe('t')
+		expect(results.makefile).toBe('myMakefile.make')
+		expect(results.dockerImage).toBe('ubuntu:18.04')
 	})
 
 	it('interprets --option=var style long options', () => {
 		const argv: readonly string[] = [
-			'0',
-			'1',
-			'2',
-			'3',
-			'--makefile=mf',
-			'--docker-image=di',
-			'--target=t',
+			'--makefile=myMakefile.make',
+			'ubuntu:18.04',
 		]
 		const results = cli.interpretArgv(argv)
-		expect(results.makefile).toBe('mf')
-		expect(results.docker_image).toBe('di')
-		expect(results.target).toBe('t')
+		expect(results.makefile).toBe('myMakefile.make')
+		expect(results.dockerImage).toBe('ubuntu:18.04')
 	})
 
 	it('interprets "--option var" style long options', () => {
 		const argv: readonly string[] = [
-			'0',
-			'1',
-			'2',
-			'3',
 			'--makefile',
-			'mf',
-			'--docker-image',
-			'di',
-			'--target',
-			't',
+			'myMakefile.make',
+			'ubuntu:18.04',
 		]
 		const results = cli.interpretArgv(argv)
-		expect(results.makefile).toBe('mf')
-		expect(results.docker_image).toBe('di')
-		expect(results.target).toBe('t')
+		expect(results.makefile).toBe('myMakefile.make')
+		expect(results.dockerImage).toBe('ubuntu:18.04')
 	})
 
-	it('sets correct default options', () => {
-		const argv: readonly string[] = ['0', '1']
+	it('interprets first positional argument as container name', () => {
+		const argv: readonly string[] = ['ubuntu:latest']
 		const results = cli.interpretArgv(argv)
-		expect(results.makefile).toBe('Makefile')
-		expect(results.docker_image).toBe('ubuntu:18.04')
-		expect(results.target).toBeNull()
+		expect(results.dockerImage).toBe('ubuntu:latest')
+	})
+
+	it('interprets additional positional arguments as target names', () => {
+		const argv: readonly string[] = ['ubuntu:latest', 'all', 'clean']
+		const results = cli.interpretArgv(argv)
+		expect(results.dockerImage).toBe('ubuntu:latest')
+		expect(results.targets.sort()).toStrictEqual(['all', 'clean'].sort())
+	})
+
+	it('errors when given no positional arguments', () => {
+		const argv: readonly string[] = []
+		expect(() => {
+			cli.interpretArgv(argv)
+		}).toThrow(Error)
+	})
+
+	it('correctly handles missing positional arguments', () => {
+		const argv: readonly string[] = ['ubuntu:18.04']
+		const results = cli.interpretArgv(argv)
+		expect(results.dockerImage).toBe('ubuntu:18.04')
+		expect(results.targets).toEqual([])
+		expect(results.makefile).toBeUndefined()
 	})
 })
