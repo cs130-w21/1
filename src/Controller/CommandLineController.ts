b4f6a1b67364d5d1d2624235b25a94c235373351
Yash Lala <yashlala@gmail.com>
2021-02-27 13:36:18 -0800

    Call `process.exit()` when given invalid CLI arguments.

--- src/Controller/CommandLineController.ts
+++ src/Controller/CommandLineController.ts
@@ -2,13 +2,25 @@ import yargs from 'yargs'
 
 export interface JunknetArguments {
 	makefile: string | undefined
-	dockerImage: string | undefined
+	dockerImage: string
 	targets: string[]
 }
 
 /**
  * Interpret a list of user-provided CLI arguments, returning the relevant ones
  * in a compact form.
+ *
+ * This function is rife with side effects. `yargs` prints to console and exits
+ * whenever the `--help` and `--version` options are invoked. It also exits
+ * when given invalid options (eg. `--doesntexist`), and reads `package.json`
+ * to find the package's current version.
+ *
+ * This is not good; it makes testing invalid options impossible
+ * (process.exit() bypasses jest entirely; all failure modes in this function
+ * are untested), and breaks encapsulation.
+ *
+ * After a long battle, our code has conceded to this approach; we exit when
+ * given invalid positional arguments.
  *
  * @param argv - A list of option arguments to parse. Be mindful that node.js
  * prepends extra arguments to process.argv; if using this function from node,
@@ -33,13 +45,14 @@ export function interpretArgv(argv: readonly string[]): JunknetArguments {
 				desc: 'The Makefile to process',
 			},
 		})
+		.version() // SIDE EFFECT: parses package.json for version info.
 		.help().argv
 
-	const positionalArguments = yargsArgv._ as string[]
+	const positionalArgs = yargsArgv._ as string[]
 
 	let dockerImage
 	const targets: string[] = []
-	for (const arg of positionalArguments) {
+	for (const arg of positionalArgs) {
 		if (dockerImage === undefined) {
 			dockerImage = arg
 		} else {
@@ -48,7 +61,12 @@ export function interpretArgv(argv: readonly string[]): JunknetArguments {
 	}
 
 	if (dockerImage === undefined) {
-		throw Error('Not enough arguments')
+		// SIDE EFFECT: prints to console and exits.
+		console.log(
+			'Not enough arguments: provide a docker image name' +
+				'(eg. "ubuntu:latest")\nTry `junknet --help` for more info',
+		)
+		process.exit(1)
 	}
 
 	return {
