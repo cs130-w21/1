34c829cc03464182600ab4473f595e7ec3d69b61
Yash Lala <yashlala@gmail.com>
2021-03-02 11:04:03 -0800

    Complete CLI Controller tests.

--- test/unit/CommandLineController.test.ts
+++ test/unit/CommandLineController.test.ts
@@ -2,15 +2,6 @@
  * Tests for the Command-Line Argument Parser
  */
 
-// When yargs encounters an invalid option (eg. -z), it'll automatically invoke
-// process.exit(). This is a problem, because it would muck up our testing
-// framework. We could handle this by explicitly passing a fail() callback to
-// yargs, but this would override the very-nice default handler. In short,
-// testing this scenario would require a lot of code.
-//
-// I don't think that the effort is worth it, because our code is just a
-// wrapper around yargs anyways (I trust their tests).
-
 import * as cli from '../../src/Controller/CommandLineController'
 
 describe('CommandLineController', () => {
@@ -19,6 +10,8 @@ describe('CommandLineController', () => {
 		const results = cli.interpretArgv(argv)
 		expect(results.makefile).toBe('myMakefile.make')
 		expect(results.dockerImage).toBe('ubuntu:18.04')
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
 	})
 
 	it('interprets --option=var style long options', () => {
@@ -29,6 +22,8 @@ describe('CommandLineController', () => {
 		const results = cli.interpretArgv(argv)
 		expect(results.makefile).toBe('myMakefile.make')
 		expect(results.dockerImage).toBe('ubuntu:18.04')
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
 	})
 
 	it('interprets "--option var" style long options', () => {
@@ -40,33 +35,47 @@ describe('CommandLineController', () => {
 		const results = cli.interpretArgv(argv)
 		expect(results.makefile).toBe('myMakefile.make')
 		expect(results.dockerImage).toBe('ubuntu:18.04')
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
 	})
 
 	it('interprets first positional argument as container name', () => {
 		const argv: readonly string[] = ['ubuntu:latest']
 		const results = cli.interpretArgv(argv)
 		expect(results.dockerImage).toBe('ubuntu:latest')
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
 	})
 
-	it('interprets additional positional arguments as target names', () => {
+	it('interprets second, third... positional arguments as target names', () => {
 		const argv: readonly string[] = ['ubuntu:latest', 'all', 'clean']
 		const results = cli.interpretArgv(argv)
 		expect(results.dockerImage).toBe('ubuntu:latest')
 		expect(results.targets.sort()).toStrictEqual(['all', 'clean'].sort())
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
+	})
+
+	it('correctly handles unspecified targets', () => {
+		const argv: readonly string[] = ['ubuntu:18.04']
+		const results = cli.interpretArgv(argv)
+		expect(results.cleanExit).toBe(false)
+		expect(results.dockerImage).toBe('ubuntu:18.04')
+		expect(results.targets).toEqual([])
+		expect(results.makefile).toBeUndefined()
+		expect(results.invalidArguments).toBe(false)
+		expect(results.cleanExit).toBe(false)
 	})
 
 	it('errors when given no positional arguments', () => {
 		const argv: readonly string[] = []
-		expect(() => {
-			cli.interpretArgv(argv)
-		}).toThrow(Error)
+		expect(cli.interpretArgv(argv).invalidArguments).toBe(true)
+		expect(cli.interpretArgv(argv).cleanExit).toBe(false)
 	})
 
-	it('correctly handles missing positional arguments', () => {
-		const argv: readonly string[] = ['ubuntu:18.04']
-		const results = cli.interpretArgv(argv)
-		expect(results.dockerImage).toBe('ubuntu:18.04')
-		expect(results.targets).toEqual([])
-		expect(results.makefile).toBeUndefined()
+	it('requests a clean exit when given `--help` option', () => {
+		const argv: readonly string[] = ['--help']
+		expect(cli.interpretArgv(argv).invalidArguments).toBe(false)
+		expect(cli.interpretArgv(argv).cleanExit).toBe(true)
 	})
 })
