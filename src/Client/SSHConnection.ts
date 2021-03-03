e39617b65f1e9fd828c9618121791aacfccbcb5b
Rohit Vighne <rohit.vighne@gmail.com>
2021-03-02 19:55:03 -0800

    Add error handling so retrying works; write docs

--- src/Client/SSHConnection.ts
+++ src/Client/SSHConnection.ts
@@ -8,14 +8,47 @@ import { Job } from '../Job/Job'
 import { JobResult } from './Client'
 import { JobRequest } from '../Network'
 
-// TODO: not hard-coded
+/**
+ * @deprecated This shouldn't be hard-coded.
+ */
 const IMAGE_NAME = 'buildpack-deps:bullseye'
+
+/**
+ * SSH username used to connect. Its value is irrelevant.
+ */
 const SSH_USER = 'junknet'
 
+/**
+ * Exit status returned by an exec stream from ssh2.
+ */
+type ExitSpec = [null, string, boolean, string] | [number]
+
+/**
+ * Thrown when a remote daemon was unable to complete a job.
+ */
+class FailedJobError extends Error {}
+
+/**
+ * Convert a {@link Job} (controller world) to a {@link JobRequest} (network world).
+ * That these types don't share an interface is a historical accident.
+ * @param job - The input job.
+ * @returns The converted job request.
+ */
 function jobToRequest(job: Job): JobRequest {
 	return { image: IMAGE_NAME, target: job.getName() }
 }
 
+/**
+ * Create a new connection to a daemon using SSH.
+ * Uses SSH exec and sftp streams to trigger jobs.
+ *
+ * @remarks
+ * Currently, host identification is a no-op, so no security is guaranteed.
+ *
+ * @param host - Hostname or IP address of the daemon.
+ * @param port - Port number of the daemon on the host.
+ * @returns Connection to an SSH daemon.
+ */
 export const createSSHConnection: ConnectionFactory = async (host, port) => {
 	const conn = new Client()
 	conn.connect({ host, port, username: SSH_USER })
@@ -27,7 +60,15 @@ export const createSSHConnection: ConnectionFactory = async (host, port) => {
 			streams.stdin.pipe(stream)
 			stream.pipe(streams.stdout)
 			stream.stderr.pipe(streams.stderr)
-			const [code] = (await once(stream, 'close')) as number[]
+
+			// Type inference fails if I check `code` by name instead of index.
+			const exitSpec = (await once(stream, 'close')) as ExitSpec
+			if (exitSpec[0] === null) {
+				const [, signal, , desc] = exitSpec
+				throw new FailedJobError(`${signal}: ${desc}`)
+			}
+
+			const [code] = exitSpec
 			return { status: code }
 		},
 
