c6a5bc08291f64c70c2317674718b0e6b8dbc958
Rohit Vighne <rohit.vighne@gmail.com>
2021-03-03 23:27:11 -0800

    Use Request as test data -> more coverage for free

--- test/unit/NetParse.test.ts
+++ test/unit/NetParse.test.ts
@@ -1,38 +1,41 @@
 import { parse, unexpected } from '../../src/Network/NetParse'
-import { JobRequest } from '../../src/Network/JobRequest'
+import { Request } from '../../src/Network/Request'
 
 describe('parse', () => {
-	it('accepts a valid job request', () => {
+	it('accepts a valid request', () => {
 		// Arrange
-		const valid = { image: 'alpine:latest', target: 'all' }
+		const valid = { action: 'job', image: 'alpine:latest', target: 'all' }
 
 		// Act
-		const parsed = parse(JobRequest, JSON.stringify(valid))
+		const parsed = parse(Request, JSON.stringify(valid))
 
 		// Assert
 		expect(parsed).toStrictEqual(valid)
 	})
 
-	it('rejects an ill-typed job request', () => {
+	it('rejects an ill-typed request', () => {
 		// Arrange
-		const missing = { image: 'alpine:latest' }
-		const badType = { image: 'alpine:latest', target: 420.69 }
+		const missing = { image: 'alpine:latest', target: 'all' }
+		const badType = { action: 'job', image: 'alpine:latest', target: 420.69 }
+		const wrongTag = { action: 'get', image: 'alpine:latest', target: 'all' }
 
 		// Act
-		const missingParsed = parse(JobRequest, JSON.stringify(missing))
-		const badTypeParsed = parse(JobRequest, JSON.stringify(badType))
+		const missingParsed = parse(Request, JSON.stringify(missing))
+		const badTypeParsed = parse(Request, JSON.stringify(badType))
+		const wrongTagParsed = parse(Request, JSON.stringify(wrongTag))
 
 		// Assert
 		expect(missingParsed).toBeUndefined()
 		expect(badTypeParsed).toBeUndefined()
+		expect(wrongTagParsed).toBeUndefined()
 	})
 
-	it('rejects a syntactically invalid job request', () => {
+	it('rejects a syntactically invalid request', () => {
 		// Arrange
 		const bogus = 'hello world!'
 
 		// Act
-		const parsed = parse(JobRequest, bogus)
+		const parsed = parse(Request, bogus)
 
 		// Assert
 		expect(parsed).toBeUndefined()
