c9e204ce7bae004b2f56dcc0373f1fc8fa52b268
Rohit Vighne <rohit.vighne@gmail.com>
2021-03-04 17:09:15 -0800

    Create "constructors" for new message types
    Keep unit tests up to date

--- test/unit/ClientNetConvert.test.ts
+++ test/unit/ClientNetConvert.test.ts
@@ -1,23 +1,58 @@
-import { jobToRequest } from '../../src/Client/ClientNetConvert'
-import { JobEnv } from '../../src/Job/Job'
-import { JobRequest } from '../../src/Network/JobRequest'
+import {
+	jobToJobRequest,
+	jobToGetArtifacts,
+	jobToPushInputs,
+} from '../../src/Client/ClientNetConvert'
+import { Job } from '../../src/Job/Job'
 import { NormalJob } from '../../src/Job/NormalJob'
 
-const MOCK_REQUEST: JobRequest = Object.freeze({
-	target: 'job',
-	image: 'fake:latest',
-})
+const MOCK_TARGET = 'all'
+const MOCK_IMAGE = 'fake:latest'
 
-describe('jobToRequest', () => {
-	it('correctly converts a job to a job request', () => {
+function createMockJob(): Job {
+	return new NormalJob(MOCK_TARGET, new Set(), { dockerImage: MOCK_IMAGE })
+}
+
+describe('ClientNetConvert', () => {
+	it('correctly converts a job to a job-trigger request', () => {
 		// Arrange
-		const env: JobEnv = { dockerImage: MOCK_REQUEST.image }
-		const job = new NormalJob(MOCK_REQUEST.target, new Set(), env)
+		const job: Job = createMockJob()
 
 		// Act
-		const request = jobToRequest(job)
+		const request = jobToJobRequest(job)
 
 		// Assert
-		expect(request).toEqual(MOCK_REQUEST)
+		expect(request).toEqual({
+			action: 'job',
+			image: MOCK_IMAGE,
+			target: MOCK_TARGET,
+		})
+	})
+
+	it('correctly converts a job to a get-artifacts request', () => {
+		// Arrange
+		const job: Job = createMockJob()
+
+		// Act
+		const request = jobToGetArtifacts(job)
+
+		// Assert
+		expect(request).toEqual({
+			action: 'get',
+			files: [MOCK_TARGET],
+		})
+	})
+
+	it('correctly converts a job to a push-inputs request', () => {
+		// Arrange
+		const job: Job = createMockJob()
+
+		// Act
+		const request = jobToPushInputs(job)
+
+		// Assert
+		expect(request).toEqual({
+			action: 'put',
+		})
 	})
 })
