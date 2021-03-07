48a6a32b55777639cc4471b7a73ac61c021fd11f
Paramjot Singh <5233968+pjot726@users.noreply.github.com>
2021-03-07 12:22:25 -0800

    Fixed test case/

--- test/unit/NormalJob.test.ts
+++ test/unit/NormalJob.test.ts
@@ -117,18 +117,22 @@ describe('NormalJob', () => {
 	})
 
 	it('returns a recursively generated list of prerequisites', () => {
-		const job1 = new NormalJob('1')
-		const job2 = new NormalJob('2')
-		const job3 = new NormalJob('3')
-		const job4 = new NormalJob('4')
-		const job5 = new NormalJob('5')
-		const job6 = new NormalJob('6')
+		const environment: JobEnv = Object.freeze({ dockerImage: 'fake:latest' })
+		const job1 = new NormalJob({target: '1', commands:[], environment})
+		const job2 = new NormalJob({target: '2', commands:[], environment})
+		const job3 = new NormalJob({target: '3', commands:[], environment})
+		const job4 = new NormalJob({target: '4', commands:[], environment})
+		const job5 = new NormalJob({target: '5', commands:[], environment})
+		const job6 = new NormalJob({target: '6', commands:[], environment})
 		const prerequisites1 = new Set<Job>([job1, job2, job3])
 		const prerequisites2 = new Set<Job>([job4, job5, job6])
-		const job7 = new NormalJob('7', prerequisites1)
-		const job8 = new NormalJob('8', prerequisites2)
+		const job7 = new NormalJob({target:'7', prerequisiteJobs:prerequisites1,
+		 		commands:[], environment})
+		const job8 = new NormalJob({target:'8', prerequisiteJobs:prerequisites2,
+				commands:[], environment})
 		const prerequisites3 = new Set<Job>([job7, job8])
-		const job9 = new NormalJob('9', prerequisites3)
+		const job9 = new NormalJob({target:'9', prerequisiteJobs:prerequisites3,
+		 		commands:[], environment})
 		const expectedArray = [job1, job2, job3, job7, job4, job5, job6, job8, job9]
 		expect(job9.getDeepPrerequisitesIterable()).toEqual(expectedArray)
 	})
