162b9af8c16b1474807f2ad97151c2a182028640
Alexander Ma <alexanderzma@hotmail.com>
2021-03-07 01:23:49 -0800

    Made some test cases for TempVolume

--- test/unit/TempVolume.test.ts
+++ test/unit/TempVolume.test.ts
@@ -1,19 +1,72 @@
+import { promises as fs, existsSync } from 'fs'
+import { resolve } from 'path'
+import {
+	safeResolve,
+	createTempDir,
+	destroyTempDir,
+} from '../../src/Daemon/TempVolume'
+
 describe('safeResolve', () => {
-	it.todo('correctly resolves safe paths')
-	it.todo('rejects relative paths that escape the root')
-	it.todo('rejects absolute paths that escape the root')
-	it.todo('rejects paths will null bytes')
-	it.todo('rejects NTFS special paths')
+	it('correctly resolves safe paths', () => {
+		// test absolute path only
+		const path1 = safeResolve('/usr/bin/ls', '')
+		expect(path1).toEqual('/usr/bin/ls')
+
+		// test absolute path with file
+		const path2 = safeResolve('/usr/bin', 'ls')
+		expect(path2).toEqual('/usr/bin/ls')
+	})
+	it('rejects relative paths that escape the root', () => {
+		const resolved = safeResolve('../', 'ls')
+		expect(resolved).toBeUndefined()
+	})
+	it('rejects absolute paths that escape the root', () => {
+		const resolved = safeResolve('/../', 'ls')
+		expect(resolved).toBeUndefined()
+	})
+	it('rejects paths with null bytes', () => {
+		const resolved = safeResolve('/', '\0')
+		expect(resolved).toBeUndefined()
+	})
+	it('rejects NTFS special paths', () => {
+		const resolved = safeResolve('/', '<>:"\\|?*')
+		expect(resolved).toBeUndefined()
+	})
 })
 
 describe('createTempDir', () => {
-	it.todo('creates a tempdir within the system temp directory')
-	it.todo('ensures the tempdir can be written to')
+	let tempdir: string
+	it('creates a tempdir within the system temp directory', async () => {
+		tempdir = await createTempDir()
+		expect(existsSync(tempdir)).toBeTruthy()
+	})
+	it('ensures the tempdir can be written to', async () => {
+		const filepath = resolve(tempdir, 'test')
+
+		// create a file in the temporary directory
+		await fs.writeFile(filepath, 'hello')
+
+		// check if the file is there
+		expect(existsSync(filepath)).toBeTruthy()
+	})
 	it.todo('ensures the tempdir cannot be accessed by others')
 })
 
 describe('destroyTempDir', () => {
-	it.todo('removes the tempdir and its contents')
+	let tempdir: string
+	it('removes the tempdir and its contents', async () => {
+		// create temporary directory
+		tempdir = await createTempDir()
+
+		// create a file in the temporary directory
+		const filepath = resolve(tempdir, 'test')
+		await fs.writeFile(filepath, 'hello')
+
+		await destroyTempDir(tempdir)
+
+		expect(existsSync(filepath)).toBeFalsy()
+		expect(existsSync(tempdir)).toBeFalsy()
+	})
 	it.todo('reports failure if the directory cannot be removed')
 })
 
