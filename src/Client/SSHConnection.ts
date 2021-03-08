028676894c554c86e03664a9dc636c8a9ea7c154
Paramjot Singh <5233968+pjot726@users.noreply.github.com>
2021-03-07 17:09:58 -0800

    Prevents empty files issues

--- src/Client/SSHConnection.ts
+++ src/Client/SSHConnection.ts
@@ -52,16 +52,19 @@ export const createSSHConnection: ConnectionFactory = async (host, port) => {
 			const pushInputStream = await promisify(conn.exec.bind(conn))(putPayload)
 			// We generate list of jobs to be tarred in the stream.
 			const fileList: Array<string> = job.getDeepPrerequisitesIterable()
-			// Create tar stream.
-			const tarStream = create({}, fileList)
-			// Send tarred contents to daemon.
-			tarStream.pipe(pushInputStream)
 
-			// Return code handling.
-			const tarExitSpec = (await once(pushInputStream, 'close')) as ExitSpec
-			if (tarExitSpec[0] === null) {
-				const [, tarSignal, , tarDesc] = tarExitSpec
-				throw new FailedJobError(`${tarSignal}: ${tarDesc}`)
+			if (fileList.length !== 0) {
+				// Create tar stream.
+				const tarStream = create({}, fileList)
+				// Send tarred contents to daemon.
+				tarStream.pipe(pushInputStream)
+
+				// Return code handling.
+				const tarExitSpec = (await once(pushInputStream, 'close')) as ExitSpec
+				if (tarExitSpec[0] === null) {
+					const [, tarSignal, , tarDesc] = tarExitSpec
+					throw new FailedJobError(`${tarSignal}: ${tarDesc}`)
+				}
 			}
 			// const [tarCode] = tarExitSpec
 
